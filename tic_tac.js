0d2f10489cbdbfdb5b7b3373a06023316c4b1d45
Salam337 <salamadash@gmail.com>
2022-11-11 19:34:17 -0500

    game logic works properly

--- tic_tac.js
+++ tic_tac.js
@@ -1,80 +1,149 @@
-const playerX = "X"
-const playerO = 'O'
-                                                                     // _______incomplete______
-let winner_p = document.querySelector('.winner > p');
-const c1 = document.getElementById('0')
-const c2 = document.getElementById('1')
-const c3 = document.getElementById('2')
-const c4 = document.getElementById('3')
-const c5 = document.getElementById('4')
-const c6 = document.getElementById('5')
-const c7 = document.getElementById('6')
-const c8 = document.getElementById('7')
-const c9 = document.getElementById('8')
+// game board
+const gameBoard = document.querySelector('#game-board')
+// message board
+const messageBoard = document.querySelector('#message-board')
+messageBoard.innerHTML = 'X Starts, GO!'
+//reset button
+const resetBtn = document.querySelector('#reset')
+let isPlayerOne = true
+let currentPlayedBoard = ['', '', '', '', '', '', '', '', '']
+let isGameOver = false
+let moveCount = 0
+const checkWin = () => {
+	const winX1 =
+		currentPlayedBoard[`cell${0}`] === 'x' &&
+		currentPlayedBoard[`cell${1}`] === 'x' &&
+		currentPlayedBoard[`cell${2}`] === 'x'
+	const winX2 =
+		currentPlayedBoard[`cell${0}`] === 'x' &&
+		currentPlayedBoard[`cell${3}`] === 'x' &&
+		currentPlayedBoard[`cell${6}`] === 'x'
+	const winX3 =
+		currentPlayedBoard[`cell${0}`] === 'x' &&
+		currentPlayedBoard[`cell${4}`] === 'x' &&
+		currentPlayedBoard[`cell${8}`] === 'x'
+	const winX4 =
+		currentPlayedBoard[`cell${1}`] === 'x' &&
+		currentPlayedBoard[`cell${4}`] === 'x' &&
+		currentPlayedBoard[`cell${7}`] === 'x'
+	const winX5 =
+		currentPlayedBoard[`cell${2}`] === 'x' &&
+		currentPlayedBoard[`cell${5}`] === 'x' &&
+		currentPlayedBoard[`cell${8}`] === 'x'
+	const winX6 =
+		currentPlayedBoard[`cell${2}`] === 'x' &&
+		currentPlayedBoard[`cell${4}`] === 'x' &&
+		currentPlayedBoard[`cell${6}`] === 'x'
+	const winX7 =
+		currentPlayedBoard[`cell${3}`] === 'x' &&
+		currentPlayedBoard[`cell${4}`] === 'x' &&
+		currentPlayedBoard[`cell${5}`] === 'x'
+	const winX8 =
+		currentPlayedBoard[`cell${6}`] === 'x' &&
+		currentPlayedBoard[`cell${7}`] === 'x' &&
+		currentPlayedBoard[`cell${8}`] === 'x'
+	const winO1 =
+		currentPlayedBoard[`cell${0}`] === 'o' &&
+		currentPlayedBoard[`cell${1}`] === 'o' &&
+		currentPlayedBoard[`cell${2}`] === 'o'
+	const winO2 =
+		currentPlayedBoard[`cell${0}`] === 'o' &&
+		currentPlayedBoard[`cell${3}`] === 'o' &&
+		currentPlayedBoard[`cell${6}`] === 'o'
+	const winO3 =
+		currentPlayedBoard[`cell${0}`] === 'o' &&
+		currentPlayedBoard[`cell${4}`] === 'o' &&
+		currentPlayedBoard[`cell${8}`] === 'o'
+	const winO4 =
+		currentPlayedBoard[`cell${1}`] === 'o' &&
+		currentPlayedBoard[`cell${4}`] === 'o' &&
+		currentPlayedBoard[`cell${7}`] === 'o'
+	const winO5 =
+		currentPlayedBoard[`cell${2}`] === 'o' &&
+		currentPlayedBoard[`cell${5}`] === 'o' &&
+		currentPlayedBoard[`cell${8}`] === 'o'
+	const winO6 =
+		currentPlayedBoard[`cell${2}`] === 'o' &&
+		currentPlayedBoard[`cell${4}`] === 'o' &&
+		currentPlayedBoard[`cell${6}`] === 'o'
+	const winO7 =
+		currentPlayedBoard[`cell${3}`] === 'o' &&
+		currentPlayedBoard[`cell${4}`] === 'o' &&
+		currentPlayedBoard[`cell${5}`] === 'o'
+	const winO8 =
+		currentPlayedBoard[`cell${6}`] === 'o' &&
+		currentPlayedBoard[`cell${7}`] === 'o' &&
+		currentPlayedBoard[`cell${8}`] === 'o'
 
+    ++moveCount
+    if (moveCount === 9 ) {
+        messageBoard.innerText = "it's a draw"
+        return true
+    }
 
-let win = () => {
- 
-    if(c1.textContent === c2.textContent && c2.textContent === c3.textContent && c1.textContent !='' ) {
-      return true;
-    }
-    else if(c4.textContent === c5.textContent && c5.textContent === c6.textContent && c4.textContent != '') {
-      return true;
-    }
-    else if(c7.textContent === c8.textContent && c8.textContent === c9.textContent && c7.textContent != '') {
-      return true;
-    }
-    else if(c1.textContent === c4.textContent && c4.textContent === c7.textContent && c1.textContent != '') {
-      return true;
-    }
-    else if(c2.textContent === c5.textContent && c5.textContent === c8.textContent && c2.textContent != '') {
-      return true;
-    }
-    else if(c3.textContent === c6.textContent && c6.textContent === c9.textContent && c3.textContent != '') {
-      return true;
-    }
-    else if(c1.textContent === c5.textContent && c5.textContent === c9.textContent && c1.textContent != '') {
-      return true;
-    }
-    else if(c3.textContent === c5.textContent && c5.textContent === c7.textContent && c3.textContent != '') {
-      return true;
-    } else {return false}
-}
-
-//
-// to start the game with first move of 'X'
-let start = () => {
-  let next = 0;
-  let cells = document.querySelectorAll('td');
-	for(let cell of cells) {
-		cell.addEventListener('click',function func() {
-			this.textContent = (['X', 'O'][next % 2]);		
-			next++;
-			this.removeEventListener('click', func);
-        if(win()) {
-         winner_p.innerHTML = this.textContent + " Won!"
-          // incomplete
-        } else if(next === 9) {
-          winner_p.innerHTML= "It's a draw"
-        //incomplete
-      }
-      })
+	if (winX1 || winX2 || winX3 || winX4 || winX5 || winX6 || winX7 || winX8) {
+		messageBoard.innerText = 'Winner is X'
+		return true
+	} else if (
+		winO1 ||
+		winO2 ||
+		winO3 ||
+		winO4 ||
+		winO5 ||
+		winO6 ||
+		winO7 ||
+		winO8
+	) {
+        messageBoard.innerText = 'Winner is O'
+        return true
 	}
 }
-  
-start()
 
+const playSpot = (event) => {
+	
+	const playedSpot = event.target.getAttribute('id')
+	const currentPlayer = isPlayerOne ? 'x' : 'o'
+    if (isGameOver) return
+	if (!currentPlayedBoard[playedSpot]) {
+        messageBoard.innerText = ''
+		isPlayerOne = !isPlayerOne
+		event.target.innerText = currentPlayer
+		currentPlayedBoard[playedSpot] = currentPlayer
+		isGameOver = checkWin()
+	} else {
+		messageBoard.innerText = 'Choose another'
+	}
 
+}
 
-// reset btn clears table starts over
-const res = () => {
-  let cell = document.getElementsByClassName('cell')
-  Array.from(cell).forEach((element) => {
-    element.innerText = ""
-    if(element.innerText == "") {
-      winner_p.innerHTML = ""
-      return start()
+const makeBoard = () => {
+
+    while(gameBoard.firstChild) {
+        gameBoard.removeChild(gameBoard.firstChild)
     }
-  })
-};
-reset.addEventListener('click', res)
+
+	for (let i = 0; i < 9; i++) {
+		
+		const playableSpot = document.createElement('div')
+		
+		playableSpot.setAttribute('id', `cell${i}`)
+		
+		playableSpot.classList.add('playableSpot')
+		
+		gameBoard.appendChild(playableSpot)
+		
+		playableSpot.addEventListener('click', playSpot)
+	}
+}
+
+const resetGame = () => {
+    isPlayerOne = true
+	currentPlayedBoard = ['', '', '', '', '', '', '', '', '']
+	isGameOver = false
+	moveCount = 0
+    messageBoard.innerText = 'X starts'
+    makeBoard()
+}
+
+document.addEventListener('DOMContentLoaded', makeBoard)
+resetBtn.addEventListener('click', resetGame)
